e4559e3abbab52a4cac351750a0ffd01d674f940
ihsanabuhanifah-fimis <abunafisaihsan@gmail.com>
2020-07-09 01:03:57 +0700

    penambahaan menu edit guru2

--- resources/views/guru/jadwal.blade.php
+++ resources/views/guru/jadwal.blade.php
@@ -22,12 +22,14 @@
         @forelse ($jadwals as $jadwal)
         <tr>
             <td class="text-center">{{$loop->iteration}}</td>
-            <td>{{$jadwal->waktu}}</td>
+            <td>
+                
+            {{$jadwal->waktu}}</td>
             <td class="text-center">{{$jadwal->jam_ke}}</td>
             <td>{{$jadwal->subject_name}}</td>
             <td>{{$jadwal->nama_rombel}}</td>
           
-            <td class="text-center">{{$jadwal->duration}}</td>
+            <td class="text-center">{{$jadwal->duration}} x {{$jadwal->perjam}} Menit</td>
             <td class="text-center">Semester {{$jadwal->semester}}</td>
             <td class="text-center">{{$jadwal->nama_ta}}</td>
             <td class="text-center"><a class="btn btn-success" href="{{route('absensi',['id'=>$jadwal->no])}}">Masuk</a></td>
